4ab1e289bd8632650ab194ad50622e7bf08086ae
ThanTharWintHtal <S10274395@connect.np.edu.sg>
2026-07-10 10:24:06 +0800

    Update for Check Point 1

---
+++ models/stallModel.js
@@ -0,0 +1,49 @@
+const { sql, poolPromise } = require('../config/db');
+
+// GET all stalls
+async function getAllStalls() {
+    const pool = await poolPromise;
+    const result = await pool.request()
+        .input('id', sql.int, id)
+        .query('SELECT * FROM Stall WHERE StallID = @id');
+    return result.recordset[0];
+}
+
+// CREATE a new stall
+async function createStall(stall) {
+    const pool = await poolPromise;
+    const result = await pool.request()
+        .input('StallName', sql.NVarChar, stall.stallName)
+        .input('CuisineType', sql.NVarChar, stall.cuisineType)
+        .input('CentreID', sql.Int, stall.centreId)
+        .input('OwnerID', sql.Int, stall.ownerId)
+        .query(`INSERT INTO Stall (StallName, CuisineType, CentreID, OwnerID)
+                OUTPUT INSERTED.*
+                VALUES (@StallName, @CuisineType, @CentreID, @OwnerID)`);
+    return result.recordset[0];
+}
+
+// UPDATE an existing stall
+async function updateStall(id, stall) {
+    const pool = await poolPromise;
+    const result = await pool.request()
+        .input('id', sql.Int, id)
+        .input('StallName', sql.NVarChar, stall.stallName)
+        .input('CuisineType', sql.NVarChar, stall.cuisineType)
+        .query(`UPDATE Stall
+                SET StallName = @StallName, CuisineType = @CuisineType
+                OUTPUT INSERTED.*
+                WHERE StallID = @id`);
+    return result.recordset[0];
+}
+
+// DELETE a stall
+async function deleteStall(id) {
+    const pool = await poolPromise;
+    await pool.request()
+        .input('id',sql.Int, id)
+        .query('DELETE FROM Stall WHERE StallID = @id');
+    return true;
+}
+
+module.exports = {getAllStalls, getStallById, createStall, updateStall, deleteStall};
