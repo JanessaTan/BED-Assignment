802bfd2f3366b91d160cdf6554a30de265087c24
ThanTharWintHtal <S10274395@connect.np.edu.sg>
2026-07-17 21:28:19 +0800

    Update

--- dbConfig.js
+++ dbConfig.js
@@ -1,3 +1,15 @@
+module.exports = {
+  user: "hygienerecord_user",
+  password: "123456",
+  server: "localhost",
+  database: "HawkerCentreHygieneRating",
+  trustServerCertificate: true,
+  options: {
+    port: 1433, // Default SQL Server port
+    connectionTimeout: 60000, // Connection timeout in milliseconds
+  },
+};
+
 const sql = require('mssql');
 require('dotenv').config();
 
