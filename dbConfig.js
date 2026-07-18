7f2e00a24fce1b71eae85027f746104381331dd9
JanessaTan <s10272516@connect.np.edu.sg>
2026-07-18 12:04:16 +0800

    collective initial login (all old versions are commented below)

--- dbConfig.js
+++ dbConfig.js
@@ -1,14 +1,26 @@
 module.exports = {
-  user: "hygienerecord_user",
+  user: "HCMS_user",
   password: "123456",
   server: "localhost",
-  database: "HawkerCentreHygieneRating",
+  database: "HawkerCentreManagementSystem",
   trustServerCertificate: true,
   options: {
     port: 1433, // Default SQL Server port
     connectionTimeout: 60000, // Connection timeout in milliseconds
   },
 };
+
+// module.exports = {
+//   user: "hygienerecord_user",
+//   password: "123456",
+//   server: "localhost",
+//   database: "HawkerCentreHygieneRating",
+//   trustServerCertificate: true,
+//   options: {
+//     port: 1433, // Default SQL Server port
+//     connectionTimeout: 60000, // Connection timeout in milliseconds
+//   },
+// };
 
 // const sql = require('mssql');
 // require('dotenv').config();
