2f6ccec61550fc4880bdbd1e955380bdec93b3cc
JanessaTan <s10272516@connect.np.edu.sg>
2026-07-19 17:54:52 +0800

    updated auth.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const { verifyJWT } = require("./middlewares/auth");
+
 // ===== You guys can update your routes here =====
 
 const authRoutes = require('./routes/authRoutes');
