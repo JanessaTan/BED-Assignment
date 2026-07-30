87b7aad7e3e5ed607c23f98c1f5c509f97e19d45
Denise-03 <03denisechew@gmail.com>
2026-07-30 23:51:10 +0800

    temp save

--- routes/salesRoutes.js
+++ routes/salesRoutes.js
@@ -1,12 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-<<<<<<< HEAD
 const salesDashboardController = require("../controllers/salesController");
-=======
-const salesController =
-require("../controllers/salesController");
->>>>>>> 2e5f3d7a590481cab95409cae266bd6840c0d32f
 
 router.get("/", salesDashboardController.getDashboard);
 
